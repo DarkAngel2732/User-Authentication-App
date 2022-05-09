3bba553d67b0f4a3fa35c400c6f51dcdd0b2c9ea
DarkAngel2732 <matt.lailey@outlook.com>
2022-05-09 11:40:20 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My Library</title>
+    </head>
+    <body>
+        <h1>Login</h1>
+        <form>
+            <label for="uName">User Name:</label>
+            <input type="text" name="uName"><br>
+
+
+            <label for="pWord">Password:</label>
+            <input type="password" name="pWord">
+
+
+
+        </form>
+    </body>
+</html>
