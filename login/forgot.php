dc02ecb7dfeb7c3727f696b00183710324d45209
DarkAngel2732 <matt.lailey@outlook.com>
2022-05-12 07:59:21 +0200

    sign up and forgot pages started
    Still need to add database functionality, will add on home PC
    
    To do:
    -finish up sign up and forgot
    -search bar for library
    -sort for library
    -look and feel

--- login/forgot.php
+++ login/forgot.php
@@ -1,9 +1,50 @@
+<?php
+//require_once "C:\wamp64\www\UserAuthenticationApp\User-Authentication-App\config.php";
+
+?>
 <!DOCTYPE html>
 <html>
-    <head>
 
-    </head>
-    <body>
-        
-    </body>
+<head>
+    Forgot password?
+</head>
+
+<body>
+    <?php if (!$_POST) { ?>
+        <form method="POST">
+            <p>If you've forgotten your password, please enter your username and select your role</p>
+
+            <label for="username">Username:</label>
+            <input type="text" name="username"><br>
+
+            <p>Select a role:</p>
+            <input type="radio" id="member" name="permission" value="member">
+            <label for="member">member</label><br>
+
+            <input type="radio" id="librarian" name="permission" value="librarian">
+            <label for="librarian">Librarian</label><br><br>
+
+            <input type="submit" value="continue">
+        </form>
+        <?php } else {
+
+        $username = $_POST['username'];
+        $permission = $_POST['permission'];
+
+        //sql select statement here
+
+        if ($username == $tempu and $permission == $tempp) {
+
+        } else {
+        ?>
+            <form method="POST">
+                <p>This user does not exist, please enter a valid username and permission</p>
+                <input type="submit" value="back">
+            </form>
+    <?php
+        }
+    }
+    ?>
+</body>
+
 </html>
--- login/sign-up.php
+++ login/sign-up.php
@@ -1,9 +1,56 @@
+<?php
+//require_once "C:\wamp64\www\UserAuthenticationApp\User-Authentication-App\config.php";
+
+?>
 <!DOCTYPE html>
 <html>
     <head>
-
+        <h1>Sign Up</h1>
     </head>
     <body>
-        
+        <?php if(!$_POST) { ?>
+        <form method="POST">
+            <label for="nUsername">New Username:</label>
+            <input type="text" name="nUsername" id="nUsername"><br>
+
+            <label for="nPassword">New Password:</label>
+            <input type="password" name="nPassword" id="nPassword"><br>
+
+            <label for="cPassword">Confirm Password:</label>
+            <input type="password" name="cPassword" id="cPassword"><br>
+
+            <p>Select a role:</p>
+            <input type="radio" id="member" name="permission" value="member">
+            <label for="member">member</label><br>
+
+            <input type="radio" id="librarian" name="permission" value="librarian">
+            <label for="librarian">Librarian</label><br><br>
+
+            <input type="submit" value="Sign Up">
+
+        </form>
+        <?php } else {
+            $nUsername = $_POST['nUsername'];
+            $nPassword = $_POST['nPassword'];
+            $cPassword = $_POST['cPassword'];
+            $permission = $_POST['permission'];
+            if($nPassword == $cPassword){
+                //sql code goes here
+
+                ?>
+            <form action="sign-in.php">
+                <p>You are signed up. Click continue to login</p>
+                <input type="submit" value="continue">
+            </form>
+                <?php
+            }else{
+                echo "<form method='post'>
+                    <p>Please confirm your password</p>
+                    <input type='submit'>
+                </form>";
+            }
+
+        }
+        ?>    
     </body>
 </html>
