57488c0289900f8769203711309be35037fd39f7
DarkAngel2732 <matt.lailey@outlook.com>
2022-05-13 18:37:00 +0200

    minor improvements

--- catalogue.php
+++ catalogue.php
@@ -17,37 +17,23 @@
         </head>
 
         <body>
-            <form method="POST">
-                Sort by:
-                <input type="submit" value="bookID" name="sort">
-                <input type="submit" value="books" name="sort">
-                <input type="submit" value="authors" name="sort">
-                <input type="submit" value="genre" name="sort">
-            </form>
+
             <?php
-            if ($_POST['sort']) {
-                $sort = $_POST['sort'];
-                switch ($sort) {
-                    case 'bookID':
-                        $sort = ' ORDER BY book_id';
-                        break;
-                    case 'books':
-                        $sort = ' ORDER BY book_name';
-                        break;
-                    case 'authors':
-                        $sort = ' ORDER BY author_name';
-                        break;
-                    case 'genre':
-                        $sort = ' ORDER BY genre';
-                        break;
-                }
-            }
+            //books table
             $sql = "SELECT * FROM books LEFT JOIN authors ON books.author_id = authors.author_id $sort;";
             $result = $db->query($sql);
 
             if ($result) {
                 if ($result->num_rows > 0) {
                     echo "<table border=1>";
+                    echo "<tr>";
+                    echo "<td>Book ID</td>";
+                    echo "<td>Book Name</td>";
+                    echo "<td>Year Published</td>";
+                    echo "<td>Book Genre</td>";
+                    echo "<td>Booke Age Group</td>";
+                    echo "<td>Author Name</td>";
+                    echo "<tr>";
                     while ($row = $result->fetch_assoc()) {
 
                         echo "<tr>";
@@ -56,6 +42,32 @@
                         echo "<td>" . $row['year'] . "</td>";
                         echo "<td>" . $row['genre'] . "</td>";
                         echo "<td>" . $row['age_group'] . "</td>";
+                        echo "<td>" . $row['author_name'] . "</td>";
+                        echo "<tr>";
+                    }
+                    echo "</table><br>";
+                }
+            } else {
+                echo "Error selecting table " . $conn->error;
+            }
+
+
+            //authors table
+            $sql = "SELECT * FROM authors;";
+            $result = $db->query($sql);
+
+            if ($result) {
+                if ($result->num_rows > 0) {
+                    echo "<table border=1>";
+                    echo "<tr>";
+                    echo "<td>Author ID</td>";
+                    echo "<td>Author Name</td>";
+                    echo "<td>Author Age</td>";
+                    echo "<td>Author's Writing Genre</td>";
+                    echo "<tr>";
+                    while ($row = $result->fetch_assoc()) {
+
+                        echo "<tr>";
                         echo "<td>" . $row['author_id'] . "</td>";
                         echo "<td>" . $row['author_name'] . "</td>";
                         echo "<td>" . $row['age'] . "</td>";
@@ -67,24 +79,27 @@
             } else {
                 echo "Error selecting table " . $conn->error;
             }
+
+
             //CRUD operations start here
+            //Add book
             ?>
             <form method="POST">
                 Add book:<br>
-                <label for="bookName">Book Name</label>
-                <input type="text" id="bookName" name="bookName" required>
+                <label for="bookName">Book Name:</label>
+                <input type="text" id="bookName" name="bookName" required><br>
 
-                <label for="year">Year</label>
-                <input type="text" id="year" name="year" required>
+                <label for="year">Year:</label>
+                <input type="text" id="year" name="year" required><br>
 
-                <label for="genre">Genre</label>
-                <input type="text" id="genre" name="genre" required>
+                <label for="genre">Genre:</label>
+                <input type="text" id="genre" name="genre" required><br>
 
-                <label for="ageGroup">Age-group</label>
-                <input type="text" id="ageGroup" name="ageGroup" required>
+                <label for="ageGroup">Age-group:</label>
+                <input type="text" id="ageGroup" name="ageGroup" required><br>
 
-                <label for="authorID">Author ID</label>
-                <input type="text" id="authorID" name="authorID" required>
+                <label for="authorID">Author ID:</label>
+                <input type="text" id="authorID" name="authorID" required><br>
 
                 <input type="submit">
             </form><br>
@@ -104,17 +119,20 @@
                     echo "Error: " . $sql . "<br>" . $conn->error;
                 }
             }
+
+
+            //Add Author
             ?>
             <form method="POST">
                 Add Author:<br>
-                <label for="authorName">Author Name</label>
-                <input type="text" id="authorName" name="authorName" required>
+                <label for="authorName">Author Name:</label>
+                <input type="text" id="authorName" name="authorName" required><br>
 
-                <label for="age">Age</label>
-                <input type="text" id="age" name="age" required>
+                <label for="age">Age:</label>
+                <input type="text" id="age" name="age" required><br>
 
-                <label for="writingGenre">Writing-genre</label>
-                <input type="text" id="writingGenre" name="writingGenre" required>
+                <label for="writingGenre">Writing-genre:</label>
+                <input type="text" id="writingGenre" name="writingGenre" required><br>
 
                 <input type="submit">
             </form><br>
@@ -132,61 +150,113 @@
                     echo "Error: " . $sql . "<br>" . $conn->error;
                 }
             }
+
+
+            //Delete Book
             ?>
             <form method="POST">
                 Delete Book: <br>
-                <label for="bookID">Book ID:</label>
-                <input type="text" id="bookID" name="bookID" required>
+                <label for="deleteBook">Book ID:</label>
+                <input type="text" id="deleteBook" name="deleteBook" required><br>
 
                 <input type="submit">
             </form><br>
             <?php
-            $bookID = $_POST['bookID'];
+            if ($_POST['deleteBook']) {
+                $deleteBook = $_POST['deleteBook'];
 
-            $sql = "DELETE FROM books WHERE book_id = $bookID";
+                $sql = "DELETE FROM books WHERE book_id = $deleteBook";
 
-            if ($db->query($sql) === TRUE) {
-                echo "Record deleted successfully";
-            } else {
-                echo "Error deleting record: " . $conn->error;
+                if ($db->query($sql) === TRUE) {
+                    echo "Record deleted successfully";
+                } else {
+                    echo "Error deleting record: " . $conn->error;
+                }
             }
 
+
+            //Delete Author
             ?>
             <form method="POST">
                 Delete Author: <br>
-                <label for="authorID">Author ID:</label>
-                <input type="text" id="authorID" name="authorID" required>
+                <label for="deleteAuthor">Author ID:</label>
+                <input type="text" id="deleteAuthor" name="deleteAuthor" required><br>
 
                 <input type="submit">
             </form><br>
             <?php
-            $authorID = $_POST['authorID'];
+            if ($_POST['deleteAuthor']) {
+                $deleteAuthor = $_POST['authorID'];
 
-            $sql = "DELETE FROM authors WHERE author_id = $authorID";
+                $sql = "DELETE FROM authors WHERE author_id = $deleteAuthor";
 
-            if ($db->query($sql) === TRUE) {
-                echo "Record deleted successfully";
-            } else {
-                echo "Error deleting record: " . $conn->error;
+                if ($db->query($sql) === TRUE) {
+                    echo "Record deleted successfully";
+                } else {
+                    echo "Error deleting record: " . $conn->error;
+                }
             }
+
+            //Update Book
+            //Update books SET "column" = "new value" WHERE book_id = ""
             ?>
             <form method="POST">
                 Update Book: <br>
-                <label for="col">Column</label>
-                <input type="text" id="col" name="col" required>
+                <label for="column">Column:</label>
+                <select name="column" id="column" required>
+                    <option value="">Select an option</option>
+                    <option value="book_id">Book ID</option>
+                    <option value="book_name">Book Name</option>
+                    <option value="year">Year Published</option>
+                    <option value="genre">Book Genre</option>
+                    <option value="age_group">Book Age Group</option>
+                    <option value="author_id">Author ID</option>
+                    <option value="author_name">Author Name</option>
+                    <option value="age">Author Age</option>
+                    <option value="writing_genre">Author's writng genre</option>
+                </select><br>
 
-                <label for="val">New Value</label>
-                <input type="text" id="val" name="val" required>
+                <label for="bookIDChange">ID of book you want to change:</label>
+                <input type="text" id="bookIDChange" name="bookIDChange" required><br>
+
+                <label for="newValue">New Value:</label>
+                <input type="text" id="newValue" name="newValue" required><br>
 
                 <input type="submit">
             </form><br>
             <?php
+            if (!$_POST['bookIDChange']) {
+                $column = $_POST['column'];
+                $bookIDChange = $_POST['bookIDChange'];
+                $newValue = $_POST['newValue'];
 
+                $sql = "DELETE FROM authors WHERE author_id = $deleteAuthor";
+
+                if ($db->query($sql) === TRUE) {
+                    echo "Record deleted successfully";
+                } else {
+                    echo "Error deleting record: " . $conn->error;
+                }
+            }
+
+
+            //Update Author
             ?>
             <form method="POST">
                 Update Author: <br>
-                <label for="col">Column</label>
-                <input type="text" id="col" name="col" required>
+                <label for="column">Column:</label>
+                <select name="column" id="column" required>
+                    <option value="">Select an option</option>
+                    <option value="book_id">Book ID</option>
+                    <option value="book_name">Book Name</option>
+                    <option value="year">Year Published</option>
+                    <option value="genre">Book Genre</option>
+                    <option value="age_group">Book Age Group</option>
+                    <option value="author_id">Author ID</option>
+                    <option value="author_name">Author Name</option>
+                    <option value="age">Author Age</option>
+                    <option value="writing_genre">Author's writng genre</option>
+                </select><br>
 
                 <label for="val">New Value</label>
                 <input type="text" id="val" name="val" required>
