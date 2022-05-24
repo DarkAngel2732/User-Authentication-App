c45ecdc383f1956098f626aa13d5254a26d540f6
DarkAngel2732 <matt.lailey@outlook.com>
2022-05-24 20:02:56 +0200

    Update catalogue.php

--- catalogue.php
+++ catalogue.php
@@ -10,6 +10,7 @@
 
 <body>
     <?php
+    ob_start();
     session_start();
     require_once "C:\wamp64\www\UserAuthenticationApp\User-Authentication-App\config.php";
     error_reporting(E_ERROR | E_WARNING | E_PARSE);
@@ -23,7 +24,6 @@
         </head>
 
         <body>
-
             <?php
             //books table
             $sql = "SELECT * FROM books LEFT JOIN authors ON books.author_id = authors.author_id $sort;";
@@ -85,10 +85,6 @@
             } else {
                 echo "Error selecting table " . $conn->error;
             }
-
-
-            //CRUD operations start here
-            //Add book
             ?>
             <!-- Form gathering information to add a book -->
             <form method="POST">
@@ -137,9 +133,6 @@
                     echo "Error: " . $sql . "<br>" . $conn->error;
                 }
             }
-
-
-            //Add Author
             ?>
             <form method="POST">
                 Add Author:<br>
@@ -169,9 +162,6 @@
                     echo "Error: " . $sql . "<br>" . $conn->error;
                 }
             }
-
-
-            //Delete Book
             ?>
             <form method="POST">
                 Delete Book:<br>
@@ -186,16 +176,15 @@
 
                 $sql = "DELETE FROM books WHERE book_id = $deleteBook";
 
+                header("refresh: 0");
+
                 if ($db->query($sql) === TRUE) {
                     echo "Record deleted successfully";
                 } else {
                     echo "Error deleting record: " . $conn->error;
                 }
-                header("refresh: 0");
+                //header("refresh: 0");
             }
-
-
-            //Delete Author
             ?>
             <form method="POST">
                 Delete Author:<br>
@@ -217,8 +206,6 @@
                 }
                 header("refresh: 0");
             }
-
-            //Update Book
             ?>
             <!-- Form gathering information to update a book -->
             <form method="POST">
@@ -267,12 +254,12 @@
                     echo "Error: " . $sql . "<br>" . $conn->error;
                 }
             }
-
-
-            //Update Author
             ?>
             <form method="POST">
                 Update Author:<br>
+                <label for="AuthorIDChange">ID of the Author you want to change:</label>
+                <input type="text" id="AuthorIDChange" name="AuthorIDChange" required><br>
+
                 <label for="column">Column:</label>
                 <select name="column" id="column" required>
                     <option value="">Select an option</option>
@@ -281,9 +268,6 @@
                     <option value="age">Age</option>
                     <option value="writing_genre">Writing Genre</option>
                 </select><br>
-
-                <label for="AuthorIDChange">ID of the Author you want to change:</label>
-                <input type="text" id="AuthorIDChange" name="AuthorIDChange" required><br>
 
                 <label for="newValue">New Value:</label>
                 <input type="text" id="newValue" name="newValue" required><br>
@@ -299,25 +283,24 @@
 
                 $sql = "UPDATE authors SET $column = '$newValue' WHERE author_id = $AuthorIDChange";
 
-                header("refresh: 0");
                 if ($db->query($sql) === TRUE) {
                     echo "Record updated successfully";
                 } else {
                     echo "Error updating record: " . $conn->error;
                 }
+                header("refresh: 0");
             }
-
             ?>
         </body>
     <?php
 
     } else {
-    ?>
-        <h1>Error, you are not signed in as a librarian. Would you like to sign-in?</h1>
+    ?><h1>Error, you are not signed in as a librarian. Would you like to sign-in?</h1>
         <form action="http://www.userauthenticationapp.com/">
             <input type="submit" value="sign in">
         </form>
     <?php
-    } ?>
+    }
+    ?>
 
 </html>
